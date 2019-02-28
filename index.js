7374c985d493fc43d717735d0afd30d360c7c253
kevin ferchaud <kevin.ferchaud@epitech.eu>
2019-02-28 15:44:29 +0100

    correct error promise and correct file index

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const planning = require('./json/index.js')();
 function refreshPlanning() {
 	setInterval(() => {
 		planning.getPlanning()
-		.then((p) => {
-		return Promise.all([led.init()])
-			.then((led) => {
-			return light.setLight(p, led)
-			})
-		})
 		.catch((err) => {
 		console.log(err)
 		});
--- json/index.js
+++ json/index.js
@@ -7,32 +7,28 @@ const token = process.env.EPILIGHTTOKEN;
 module.exports = () => {
     return {
         getPlanning : () => {
-            return Promise.all([api.getUser(token)])
-		.then((user) => api.getPlanning(token, user[0]))
-		.then((planning) => {
+                return Promise.all([api.getUser(token)])
+		    .then((user) => api.getPlanning(token, user[0]))
+		    .then((planning) => {
                 const rooms = api.getRoom(planning);
                 let json = [];
-
-		    Object.values(rooms).forEach((roomName) => {
-			json.push(  {start : roomName.start, end: roomName.end, room: roomName.room.code.split('/')[3]});	
-		    });
-            Object.values(config).forEach((roomName) => {
-			if (api.getRoomIdx(json, roomName) == -1)
-                            json.push(  {start : new Date(), end: new Date(), room: roomName.name});	
-            });
-            fs.writeFile('room.json', JSON.stringify(json), 'utf8', (err, data) => {
-                if (err) throw err;
-            });
-
-            return json;
-		}).catch((err) => {
-		    console.log(err)
+    		    Object.values(rooms).forEach((roomName) => {
+	            	json.push({start : roomName.start, end: roomName.end, room: roomName.room.code.split('/')[3]});	
+		        });
+                Object.values(config).forEach((roomName) => {
+		            if (api.getRoomIdx(json, roomName) == -1)
+                        json.push(  {start : new Date(), end: new Date(), room: roomName.name});	
+                });
+                fs.writeFile('room.json', JSON.stringify(json), 'utf8', (err, data) => {
+                    if (err) throw err;
+                });
+                return json;
+		    }).catch((err) => {
+		        console.log(err);
                 let json = [];
-                if (err)
-			throw err;
+                if (err) throw err;
                 return fs.readFile('room.json', 'utf8', (err, data) => {
-                    if (err)
-                        throw err;
+                    if (err) throw err;
                     return JSON.parse(data);
                 })
             });
